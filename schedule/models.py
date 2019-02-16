f167c2d9ec21778a9872534848626c0296603502
navidkpr <navidkouchakipour@yahoo.com>
2019-02-16 17:36:58 -0500

    added min-max values for the time gap for Medicine

--- schedule/models.py
+++ schedule/models.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 from django.db import models
 
 # Create your models here.
 
 class Medicine(models.Model):
     name = models.CharField(max_length = 50)
-    timeGap = models.IntegerField()
+    timeGap = models.IntegerField(validators = [MinValueValidator(4), MaxValueValidator(24)])
     number = models.CharField(max_length = 20)
 
     def __str__(self):
