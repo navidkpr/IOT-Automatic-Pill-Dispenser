54cbedfe06f9c43b2252d9e99ce08170e5fc1c91
wlgfour <wlgfour@gmail.com>
2019-02-16 22:09:18 -0500

    fixed bug related to the signals - server is running on pi

--- schedule/signals.py
+++ schedule/signals.py
@@ -3,14 +3,13 @@ from django.db.models.signals import post_save
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from schedule.models import Medicine
-from cron_scheduler import create_cron, remove_cron
-
-#def create_cron(name, hour, id_):
-#    print("create_cron name: " + name + " id: " + str(id_) + " hour: " + str(hour))
+from schedule.cron_scheduler import create_cron, remove_cron 
+#def create_cron(name, hour, id_): 
+    #print("create_cron name: " + name + " id: " + str(id_) + " hour: " + str(hour))
 
 
 #def remove_cron(id_):
-#    print("remove_cron id: " + str(id_))
+    #print("remove_cron id: " + str(id_))
 
 @receiver(post_save, sender=Medicine)
 def my_handler1(sender, instance, created=False, **kwargs):
